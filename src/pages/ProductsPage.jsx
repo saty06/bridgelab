d6492bda7e1aef08b6dca057111cf6f0cc06ff73
saty06 <saty06072000@gmail.com>
2025-01-17 15:22:28 +0530

    CG Technocrat Bhopal Learners Dashboard add

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -68,7 +68,7 @@ const ProductsPage = () => {
 	}, []);
 	return (
 		<div className='flex-1 overflow-auto relative z-10'>
-			<Header title='Student Dashboard' />
+			<Header title=' CG Technocrat Bhopal Learners Dashboard' />
       
 
 			<main className='max-w-7xl mx-auto py-6 px-4 lg:px-8'>

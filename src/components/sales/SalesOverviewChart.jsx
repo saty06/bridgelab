4dd36280139daf9892148d243fb2782ced058aa3
saty06 <saty06072000@gmail.com>
2025-01-23 15:29:15 +0530

    fix the issue

--- src/components/sales/SalesOverviewChart.jsx
+++ src/components/sales/SalesOverviewChart.jsx
@@ -172,10 +172,7 @@ const StudentAbasentTable = () => {
                 value={searchTerm}
                 onChange={handleSearch}
               />
-              <Search
-                className="absolute left-3 top-2.5 text-gray-400"
-                size={18}
-              />
+             
               <button
                 className="bg-gray-700 text-white rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
                 onClick={downloadCSV}
